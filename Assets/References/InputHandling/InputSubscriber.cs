fdd8338a8c7ce80bad9a4b959a9a4b4427723b20
TylerBFabela <hamsterholic777@gmail.com>
2024-02-24 23:16:44 -0800

    Editing of Subscriber Script
    Attempted to Implement the movement of the player to the Subscriber script

--- Assets/References/InputHandling/InputSubscriber.cs
+++ Assets/References/InputHandling/InputSubscriber.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class InputSubscriber : MonoBehaviour
 {
+    // enums
+    public enum PlayerMoveDirection
+    {
+        Left,
+        Right
+    }
+
+
+    // initialize variables
+
+    private PlayerMoveDirection playerMoveDirection = PlayerMoveDirection.Right; // starting movement direction of right
+
+    private const int AMOUNT_TO_MOVE_LEFT = 1;
+    private const int AMOUNT_TO_MOVE_RIGHT = -1;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,23 +42,64 @@ public class InputSubscriber : MonoBehaviour
     public void HandleTap()
     {
         Debug.Log("Tap detected!");
+
+
+        // TODO
     }
 
     // What to do on Consecutive Tap
     public void HandleConsecutiveTap()
     {
         Debug.Log("Consecutive Tap detected!");
+
+
+        // TODO
     }
 
     // What to do when the player starts pressing
     public void HandlePress()
     {
         Debug.Log("Press detected!");
+
+        // checks which direction for the player to move 
+        if (playerMoveDirection == PlayerMoveDirection.Left) // true if the current direction for the player to move is left
+        {
+            // moves the player to the left
+
+            PlayerMovement(AMOUNT_TO_MOVE_LEFT);
+
+        }
+        else if (playerMoveDirection == PlayerMoveDirection.Right) // true if the current direction for the player to move is right
+        {
+            // moves the player to the right
+
+            PlayerMovement(AMOUNT_TO_MOVE_RIGHT);
+        }
+
     }
 
     // What to do when the player releases a press
     public void HandleRelease()
     {
         Debug.Log("Release detected!");
+
+
+        // switches direction of player movement for next press
+        if (playerMoveDirection == PlayerMoveDirection.Left) // true if the current direction for the player to move is left
+        {
+            playerMoveDirection = PlayerMoveDirection.Right; // switches the next direction to right
+        }
+        else if (playerMoveDirection == PlayerMoveDirection.Right) // true if the current direction for the player to move is right
+        {
+            playerMoveDirection = PlayerMoveDirection.Left; // switches the next direction to left
+        }
+
+    }
+
+    private void PlayerMovement(int playerMovementAmountAlongX)
+    {
+        Vector3 playerMovementVector = new Vector3(playerMovementAmountAlongX, 0f, 0f);
+
+        transform.forward = playerMovementVector;
     }
 }
--- Assets/Scripts/GenericPlayerMovement.cs
+++ Assets/Scripts/GenericPlayerMovement.cs
@@ -5,22 +5,106 @@ using UnityEngine;
 
 public class GenericPlayerMovement : MonoBehaviour
 {
-    // 
-    public event EventHandler On 
+
+    /**
+    // event for when the user taps on the screen 
+    public event EventHandler OnMoveAction;
+    public event EventHandler OnShootAction;
 
 
-    // Start is called before the first frame update
-    void Start()
+    public delegate void TapEventHandler();
+    public event TapEventHandler OnTapEvent;
+
+    public delegate void ConsecutiveTapEventHandler();
+    public event ConsecutiveTapEventHandler OnConsecutiveTapEvent;
+
+    public delegate void PressEventHandler();
+    public event PressEventHandler OnPressEvent;
+
+    public delegate void ReleaseEventHandler();
+    public event ReleaseEventHandler OnReleaseEvent;
+
+
+    // Instance of the GenericPlayerMovement
+    public static GenericPlayerMovement Instance { get; private set; }
+
+    private const string PLAYER_PREFS_BINDINGS = "InputBindings";
+
+    private string directionOfMovement = "";
+
+    //
+    public enum Binding
     {
-        
+        Move_Left,
+        Move_Right,
+        Shoot
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    private PlayerInputActions playerInputActions;
+
+
+    // Awake starts as soon as the game launches
+    private void Awake()
     {
-        
+        Instance = this;
+
+        playerInputActions = new PlayerInputActions();
+
+       
+
+
+        /*
+        if (PlayerPrefs.HasKey(PLAYER_PREFS_BINDINGS))
+        {
+            playerInputActions.LoadBindingOverridesFromJson(PlayerPrefs.GetString(PLAYER_PREFS_BINDINGS));
+        }
+        */
+
+/*
+
+        // Allows the user to make player actions
+        playerInputActions.Player.Enable();
+
+        // adds the events for player move and shoot
+        playerInputActions.Player.Move.performed += Move_performed;
+        playerInputActions.Player.Shoot.performed += Shoot_performed;
+
+    }
+
+    // As the game closes, it destroys all the playerInputActions events
+    private void OnDestroy()
+    {
+        playerInputActions.Player.Move.performed -= Move_performed;
+        playerInputActions.Player.Shoot.performed -= Shoot_performed;
+
+        playerInputActions.Dispose();
     }
 
 
 
+    private void Move_performed(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        Debug.Log(context);
+        OnMoveAction?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void Shoot_performed(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        OnShootAction?.Invoke(this, EventArgs.Empty);
+    }
+
+    // function to return the vector to represent the player's movement
+    public int GetMovementComponent()
+    {
+
+        int inputVector = playerInputActions.Player.Move.ReadValue<int>();
+
+        return inputVector;
+    }
+
+    
+
+    **/
 }
+
