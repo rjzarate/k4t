0b40de2ba48c0632cdfb1e4aa1a1bbc0d1a4685a
pham-t-alex <pham.t.alex@gmail.com>
2024-03-16 12:12:19 -0700

    Revert "Merge branch 'development' into feature-world1-prototype"
    This reverts commit 656f6034c7ea5433eb360bcdc8a9dc18e630c861, reversing
    changes made to 34df064b9e2a22c19815ce5afdd81bb417bf4cad.

--- Assets/Scripts/BossReceiveHit.cs
+++ Assets/Scripts/BossReceiveHit.cs
@@ -8,20 +8,16 @@ using UnityEngine;
 public class BossReceiveHit : MonoBehaviour, IHittable
 {
     private Health health; // boss's instance of health
-    
-    private BossHealthBarUI bossHealthBarUI; // boss's instance of health bar
 
     void Start()
     {
         health = GetComponent<Health>();
-        bossHealthBarUI = GetComponent<BossHealthBarUI>();
     }
 
 
     public void TriggerEffects(List<Effect> effects)
     {
-        foreach(Effect effect in effects) 
-        {
+        foreach(Effect effect in effects) {
             if (effect.GetEffectType() == Effect.EffectType.Damage)
             {
                 ApplyEffectDamage(effect);
@@ -29,44 +25,10 @@ public class BossReceiveHit : MonoBehaviour, IHittable
         }
     }
 
-    // function to apply effect damage and other additional effects to the boss
     private void ApplyEffectDamage(IEffectDamage effectDamage)
     {
-        // deals with the numerical health change of the boss
-        Debug.Log("Damage: " + effectDamage.GetDamage()); // output to console for testing
+        Debug.Log("Damage: " + effectDamage.GetDamage());
         GetComponent<Health>().Damage(effectDamage.GetDamage());
-        
-        // deals with the healthbar change of the boss
-        bossHealthBarUI.Damage(effectDamage.GetDamage(), health.GetMaxHealth());
-        health.Damage(effectDamage.GetDamage());
-        
-        foreach (Effect effect in effects) // applies all valid effects
-        {
-            ApplyEffect(effect);
-        }
-    }
-
-    // function to apply an effect to the boss
-    private void ApplyEffect(Effect effect)
-    {
-        switch (effect.GetEffectType())
-        {
-            case Effect.EffectType.Damage:
-                ApplyEffectDamage(effect);
-                break;
-            case Effect.EffectType.Slow:
-                ApplyEffectSlow(effect);
-                break;
-            default:
-                Debug.LogError("Unimplemented effect: " + effect.GetEffectType());
-                return;
-        }
-    }
-    
-    // function to apply slow effect to the boss
-    private void ApplyEffectSlow(IEffectSlow effectSlow)
-    {
-        throw new NotImplementedException();
     }
 }
 
