ef3f4a369f6644da1c26f9a9f63cc9e44c620bde
TylerBFabela <hamsterholic777@gmail.com>
2024-03-19 22:46:24 -0700

    Addition of Some Sounds and Music/Sound Managers

---
+++ Assets/Scenes/AudioItems/AudioClipReferencesSO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class AudioClipReferencesSO : ScriptableObject
+{
+    public AudioClip[] playerFire;
+    public AudioClip[] bossFire;
+    public AudioClip[] meateorSplat;
+
+}
