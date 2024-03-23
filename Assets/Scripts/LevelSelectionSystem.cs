e0cd024f80d7220e7f270d65a66c29591ae7ea9a
pham-t-alex <pham.t.alex@gmail.com>
2024-03-23 15:54:54 -0700

    change level buttons and animation

--- Assets/Scripts/LevelSelectionSystem.cs
+++ Assets/Scripts/LevelSelectionSystem.cs
@@ -19,6 +19,9 @@ public class LevelSelectionSystem : MonoBehaviour
     [SerializeField] private Vector2 leftAnchorMax;
     [SerializeField] private Vector2 rightAnchorMin;
     [SerializeField] private Vector2 rightAnchorMax;
+    [Header("Animation Settings")]
+    [SerializeField] private float worldMoveDuration;
+    [SerializeField] private float worldMoveOffsetTime;
 
     private void Awake()
     {
@@ -45,12 +48,34 @@ public class LevelSelectionSystem : MonoBehaviour
         {
             rightButton.GetComponent<Button>().interactable = false;
         }
+        worldButtons[currentWorld].GetComponent<Button>().interactable = true;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    // changes to previous or next level
+    public void ChangeLevel(bool rightward)
+    {
+        GameObject oldWorld = worldButtons[currentWorld];
+        if (rightward)
+        {
+            currentWorld++;
+        }
+        else
+        {
+            currentWorld--;
+        }
+        leftButton.GetComponent<Button>().interactable = false;
+        rightButton.GetComponent<Button>().interactable = false;
+        oldWorld.GetComponent<Button>().interactable = false;
+        worldButtons[currentWorld].GetComponent<Button>().interactable = false;
+        worldButtons[currentWorld].SetActive(true);
+        worldText.GetComponent<TMP_Text>().text = "";
+        StartCoroutine(ShiftWorlds(oldWorld, worldButtons[currentWorld], rightward, worldMoveDuration, worldMoveOffsetTime));
     }
 
     // changes to the previous level
@@ -93,19 +118,70 @@ public class LevelSelectionSystem : MonoBehaviour
         }
     }
 
-    // moves from initialPosition to targetPosition over moveDuration
-    private IEnumerator ShiftWorlds(GameObject oldWorldButton, GameObject newWorldButton, bool rightward, float moveDuration)
+    // shifts the worlds
+    private IEnumerator ShiftWorlds(GameObject oldWorldButton, GameObject newWorldButton, bool rightward, float moveDuration, float moveOffset)
     {
         float elapsedTime = 0f;
-
-        while (elapsedTime < moveDuration)
+        // set new world's position to the right spot
+        if (rightward)
         {
-            //transform.position = Vector2.Lerp(initialPosition, targetPosition, elapsedTime / moveDuration);
-            //fix
+            newWorldButton.GetComponent<RectTransform>().anchorMax = rightAnchorMax;
+            newWorldButton.GetComponent<RectTransform>().anchorMin = rightAnchorMin;
+        }
+        else
+        {
+            newWorldButton.GetComponent<RectTransform>().anchorMax = leftAnchorMax;
+            newWorldButton.GetComponent<RectTransform>().anchorMin = leftAnchorMin;
+        }
+
+        // shift the worlds
+        while (elapsedTime < moveDuration + moveOffset)
+        {
+            if (elapsedTime < moveDuration)
+            {
+                if (rightward)
+                {
+                    oldWorldButton.GetComponent<RectTransform>().anchorMax = Vector2.Lerp(centerAnchorMax, leftAnchorMax, elapsedTime / moveDuration);
+                    oldWorldButton.GetComponent<RectTransform>().anchorMin = Vector2.Lerp(centerAnchorMin, leftAnchorMin, elapsedTime / moveDuration);
+                }
+                else
+                {
+                    oldWorldButton.GetComponent<RectTransform>().anchorMax = Vector2.Lerp(centerAnchorMax, rightAnchorMax, elapsedTime / moveDuration);
+                    oldWorldButton.GetComponent<RectTransform>().anchorMin = Vector2.Lerp(centerAnchorMin, rightAnchorMin, elapsedTime / moveDuration);
+                }
+            }
+            else
+            {
+                oldWorldButton.SetActive(false);
+            }
+            if (elapsedTime >= moveOffset)
+            {
+                if (rightward)
+                {
+                    newWorldButton.GetComponent<RectTransform>().anchorMax = Vector2.Lerp(rightAnchorMax, centerAnchorMax, (elapsedTime - moveOffset) / moveDuration);
+                    newWorldButton.GetComponent<RectTransform>().anchorMin = Vector2.Lerp(rightAnchorMin, centerAnchorMin, (elapsedTime - moveOffset) / moveDuration);
+                }
+                else
+                {
+                    newWorldButton.GetComponent<RectTransform>().anchorMax = Vector2.Lerp(leftAnchorMax, centerAnchorMax, (elapsedTime - moveOffset) / moveDuration);
+                    newWorldButton.GetComponent<RectTransform>().anchorMin = Vector2.Lerp(leftAnchorMin, centerAnchorMin, (elapsedTime - moveOffset) / moveDuration);
+                }
+            }
             elapsedTime += Time.deltaTime;
             yield return null; // Wait until the next frame
         }
-
-        //set active and interactable
+        newWorldButton.GetComponent<RectTransform>().anchorMax = centerAnchorMax;
+        newWorldButton.GetComponent<RectTransform>().anchorMin = centerAnchorMin;
+        oldWorldButton.SetActive(false);
+        newWorldButton.GetComponent<Button>().interactable = true;
+        if (currentWorld > 0)
+        {
+            leftButton.GetComponent<Button>().interactable = true;
+        }
+        if (currentWorld < worldButtons.Count - 1)
+        {
+            rightButton.GetComponent<Button>().interactable = true;
+        }
+        worldText.GetComponent<TMP_Text>().text = newWorldButton.GetComponent<WorldButton>().WorldName;
     }
 }
