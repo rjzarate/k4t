e683e9d54a5f6a5c4c2d67af22f08e3ee1a862ac
rjzarate <zarate_rj@yahoo.com>
2024-06-21 21:38:52 -0700

    boss tint health
    boss gets more red as they have less health

---
+++ Assets/Scripts/Bosses/World1/BossHurtAnimation.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHurtAnimation : MonoBehaviour
+{
+    [SerializeField] private Health bossHealth;
+    [SerializeField] private SpriteRenderer bossSprite;
+    [SerializeField] private float showHurtDuration = 0.2f;
+
+    [SerializeField] private Color bossColorDefault = Color.white;
+    [SerializeField] private Color bossColorMinimumHurt = Color.red;
+    [SerializeField] private Color bossColorMaximumHurt = Color.red;
+
+
+    private void Start() {
+        bossHealth.TakeDamageEvent += HandleTakeDamage;
+    }
+
+    private void HandleTakeDamage(float newHealth)
+    {
+        // Find difference between max and min multiplied by health difference
+        float r = (bossColorMaximumHurt.r - bossColorMinimumHurt.r) * (bossHealth.GetMaxHealth() - newHealth) / bossHealth.GetMaxHealth();
+        float g = (bossColorMaximumHurt.g - bossColorMinimumHurt.g) * (bossHealth.GetMaxHealth() - newHealth) / bossHealth.GetMaxHealth();
+        float b = (bossColorMaximumHurt.b - bossColorMinimumHurt.b) * (bossHealth.GetMaxHealth() - newHealth) / bossHealth.GetMaxHealth();
+        float a = (bossColorMaximumHurt.a - bossColorMinimumHurt.a) * (bossHealth.GetMaxHealth() - newHealth) / bossHealth.GetMaxHealth();
+
+        bossSprite.color = new Color(r + bossColorMinimumHurt.r, g + bossColorMinimumHurt.g, b + bossColorMinimumHurt.b, a + bossColorMinimumHurt.a);
+
+        StopAllCoroutines();
+        StartCoroutine(ShowHurtRoutine(showHurtDuration));
+    }
+
+    private IEnumerator ShowHurtRoutine(float showHurtDuration)
+    {
+        yield return new WaitForSeconds(showHurtDuration);
+        bossSprite.color = bossColorDefault;
+    }
+}
