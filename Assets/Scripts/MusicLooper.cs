169ddd102111ceb466b0f1f809100f1e4b5a31c9
rjzarate <zarate_rj@yahoo.com>
2024-06-22 02:27:03 -0700

    edits for webgl build and music build tweak

--- Assets/Scripts/MusicLooper.cs
+++ Assets/Scripts/MusicLooper.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,11 +11,21 @@ public class MusicLooper: MonoBehaviour
     [SerializeField] private AudioSource loopSource;
     //private AudioSource audioSource;
     // Start is called before the first frame update
-    void Start()
+    async void Start()
     {
+
+        StartCoroutine(PlayMusicRoutine());
+        
+    }
+
+    private IEnumerator PlayMusicRoutine()
+    {
+        yield return new WaitUntil(() => introSource.clip.loadState == AudioDataLoadState.Loaded);
         double time = AudioSettings.dspTime;
-        introSource.PlayScheduled(0.5 + time);
-        loopSource.PlayScheduled(0.5 + time + introSource.clip.length);
+        // WEBGL IS WEIRD RN MAKING CLIP.LENGTH 0 NO MATTER WHAT I DO
+        // introSource.PlayScheduled(0.5 + time);
+        // loopSource.PlayScheduled(0.5 + time + introSource.clip.length);
+        loopSource.PlayScheduled(0.5 + time );
     }
 
     // Update is called once per frame
