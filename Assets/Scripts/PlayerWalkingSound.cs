8ba948281afa8ea206a05599e3b3c6af09116fef
rjzarate <zarate_rj@yahoo.com>
2024-06-24 00:25:30 -0700

    walking sound fix and sound changes
    walking sound now cuts off after playing the last note
    boss hurt sound now is only the explosion

--- Assets/Scripts/PlayerWalkingSound.cs
+++ Assets/Scripts/PlayerWalkingSound.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerWalkingSound : MonoBehaviour
 {
     private AudioSource audioSource;
+	public int sampleDataLength = 1024;
+
+	private float[] clipSampleData;
+
+    private bool isPlayerWalking = false;
 
     private void Awake()
     {
@@ -14,8 +19,32 @@ public class PlayerWalkingSound : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        clipSampleData = new float[sampleDataLength];
+
         PlayerMovement playerMovement = Player.Instance.GetPlayerMovement();
         playerMovement.OnWalkSoundEvent += PlayerMovement_OnWalkSoundEvent;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (!isPlayerWalking && audioSource.isPlaying) {
+            float clipLoudness = CalculateClipLoudness();
+            if (clipLoudness < 0.01f) {
+                audioSource.Pause();
+            }
+        }
+        
+	}
+
+    private float CalculateClipLoudness() {
+        audioSource.clip.GetData(clipSampleData, audioSource.timeSamples); //I read 1024 samples, which is about 80 ms on a 44khz stereo clip, beginning at the current sample position of the clip.
+        float clipLoudness = 0f;
+        foreach (var sample in clipSampleData) {
+            clipLoudness += Mathf.Abs(sample);
+        }
+        clipLoudness /= sampleDataLength; //clipLoudness is what you are looking for
+        return clipLoudness;
+		
     }
 
     // function used to determine when to play the player's walking sound
@@ -25,10 +54,12 @@ public class PlayerWalkingSound : MonoBehaviour
         if (playerCanMakeWalkingSound) // true if the player is allowed to move
         {
             audioSource.Play();
+            isPlayerWalking = true;
         }
         else
         {
-            audioSource.Pause();
+            // audioSource.Pause();
+            isPlayerWalking = false;
         }
     }
 }
